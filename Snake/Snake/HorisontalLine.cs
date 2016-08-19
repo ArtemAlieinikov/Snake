105ba9d750e56e796f2d8b6bf598b748e1869087
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-20 01:29:09 +0300

    created class "HorizontalLine", which is derived class from "Figure".

---
+++ Snake/Snake/HorisontalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorisontalLine : Figure
+    {
+        public HorisontalLine(int firstDot, int lastDot, int y, char symbol)
+        {
+            dotList = new List<Dot>();
+
+            for(int i = firstDot; i <= lastDot; i++)
+            {
+                dotList.Add(new Dot(i, y, symbol));
+            }
+        }
+    }
+}
