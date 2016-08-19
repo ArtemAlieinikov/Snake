23225a169f62c8d309893837f860a09c46d51fbf
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-20 01:25:07 +0300

    create a base class "Figure" to build different lines.

---
+++ Snake/Snake/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Figure
+    {
+        protected List<Dot> dotList;
+
+        public void Draw()
+        {
+            foreach(Dot currentDot in dotList)
+            {
+                currentDot.Draw();
+            }
+        }
+    }
+}
