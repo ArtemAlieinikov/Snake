ba165f6de43c1f7f84e0b906c70636da7cdcd113
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-20 01:29:33 +0300

    created class "VerticalLine", which is derived class from "Figure".

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int firstDot, int lastDot, int x, char symbol)
+        {
+            dotList = new List<Dot>();
+
+            for(int i = firstDot; i <= lastDot; i++)
+            {
+                dotList.Add(new Dot(x, i, symbol));
+            }
+        }
+    }
+}
