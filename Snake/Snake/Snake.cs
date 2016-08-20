f9cdd36617487e0d28308230f94b55a36184c574
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-20 11:21:54 +0300

    added "Move" method, and "GetNextDot" method.

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -12,12 +12,36 @@ namespace Snake
 
         public Snake(Dot tail, int length, Direction direct)
         {
+            directionOfMoving = direct;
+
             for(int i = 0; i <= length; i++)
             {
                 Dot snakeDot = new Dot(tail);
-                snakeDot.Move(i, direct);
+                snakeDot.Move(i, directionOfMoving);
                 dotList.Add(snakeDot);
             }
+        }
+
+        public void Move()
+        {
+            Dot tail = dotList.First();
+            dotList.Remove(tail);
+
+            Dot head = GetNextDot();
+            dotList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        private Dot GetNextDot()
+        {
+            Dot previousHeadDot = dotList.Last();
+            Dot nextHeadDot = new Dot(previousHeadDot);
+
+            nextHeadDot.Move(1, directionOfMoving);
+
+            return nextHeadDot;
         }
     }
 }
