5845504108bc2c225bbbe46b5647dd9925a20f51
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-21 00:24:12 +0300

    added "FoodCreator" class that inherits from the "Dot" and implement the mechanism of creating food for snakes.

---
+++ Snake/Snake/FoodCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class FoodCreator : Dot
+    {
+        Random rand = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, char symbol) 
+            : base(mapWidth, mapHeight, symbol)
+        { }
+
+        public Dot CreateFood()
+        {
+            int rX = rand.Next(2, x - 2);
+            int rY = rand.Next(2, y - 2);
+            return new Dot(rX, rY, symbol);
+        }
+    }
+}
