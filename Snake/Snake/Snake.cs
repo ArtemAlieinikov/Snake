84915c14bc414c9bdf55002db3b899fb78094f55
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-21 00:27:53 +0300

    added methods "KeyPushReaction" and "IsEat". First - change of direction by pressing keys, second - mechanism of eating food and snakes growth.

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -9,12 +9,14 @@ namespace Snake
     class Snake : Figure
     {
         Direction directionOfMoving;
+        Dot nextHeadDot;
 
         public Snake(Dot tail, int length, Direction direct)
         {
+            length = length <= 1 ? 2 : length;
             directionOfMoving = direct;
 
-            for(int i = 0; i <= length; i++)
+            for(int i = 0; i < length; i++)
             {
                 Dot snakeDot = new Dot(tail);
                 snakeDot.Move(i, directionOfMoving);
@@ -27,21 +29,70 @@ namespace Snake
             Dot tail = dotList.First();
             dotList.Remove(tail);
 
-            Dot head = GetNextDot();
-            dotList.Add(head);
+            nextHeadDot = GetNextDot();
+            dotList.Add(nextHeadDot);
 
             tail.Clear();
-            head.Draw();
+            nextHeadDot.Draw();
         }
 
         private Dot GetNextDot()
         {
             Dot previousHeadDot = dotList.Last();
-            Dot nextHeadDot = new Dot(previousHeadDot);
+            nextHeadDot = new Dot(previousHeadDot);
 
             nextHeadDot.Move(1, directionOfMoving);
 
             return nextHeadDot;
+        }
+
+        public void KeyPushReaction(ConsoleKey key)
+        {
+            if((key == ConsoleKey.W) || (key == ConsoleKey.UpArrow))
+            {
+                if(directionOfMoving != Direction.DOWN)
+                {
+                    directionOfMoving = Direction.UP;
+                }
+            }
+
+            if ((key == ConsoleKey.S) || (key == ConsoleKey.DownArrow))
+            {
+                if (directionOfMoving != Direction.UP)
+                {
+                    directionOfMoving = Direction.DOWN;
+                }
+            }
+
+            if ((key == ConsoleKey.A) || (key == ConsoleKey.LeftArrow))
+            {
+                if (directionOfMoving != Direction.RIGHT)
+                {
+                    directionOfMoving = Direction.LEFT;
+                }
+            }
+
+            if ((key == ConsoleKey.D) || (key == ConsoleKey.RightArrow))
+            {
+                if (directionOfMoving != Direction.LEFT)
+                {
+                    directionOfMoving = Direction.RIGHT;
+                }
+            }
+        }
+
+        public bool IsEat(Dot food)
+        {
+            if(nextHeadDot.IsHit(food))
+            {
+                food.symbol = nextHeadDot.symbol;
+                dotList.Add(food);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
