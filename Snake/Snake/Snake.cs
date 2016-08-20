96b398b5b675c8d5fd89a43a9563e4dd9039774d
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-20 08:34:15 +0300

    created class "Snake", which is derived class from "Figure".

---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        Direction directionOfMoving;
+
+        public Snake(Dot tail, int length, Direction direct)
+        {
+            for(int i = 0; i <= length; i++)
+            {
+                Dot snakeDot = new Dot(tail);
+                snakeDot.Move(i, direct);
+                dotList.Add(snakeDot);
+            }
+        }
+    }
+}
