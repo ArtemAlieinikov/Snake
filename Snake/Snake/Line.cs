51912bf7aa4e9dcf70dc0eac69a10745c65fe464
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-08-23 00:47:11 +0300

    refactoring, now there is "Line" class instead "HorizontalLine" and "VerticalLine" classes. Now it easier to draw the playing field and obstacles.

---
+++ Snake/Snake/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Line : Figure
+    {
+        public Line(int firstPosition, int lineLength, int HeightWidthLine, HorizontalVertical direction, char symbol)
+        {
+            if(direction == HorizontalVertical.HORIZONTAL)
+            {
+                for(int x = firstPosition; x <= lineLength; x++)
+                {
+                    dotList.Add(new Dot(x, HeightWidthLine, symbol));
+                }
+            }
+
+            else 
+            {
+                for (int y = firstPosition; y <= lineLength; y++)
+                {
+                    dotList.Add(new Dot(HeightWidthLine, y, symbol));
+                }
+            }
+        }
+    }
+}
